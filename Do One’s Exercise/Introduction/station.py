d2450636896201bc58e9b242835983b26e74a428
0E <92708940+Yum02@users.noreply.github.com>
2023-05-07 14:10:49 +0000

    변수

---
+++ Do One’s Exercise/Introduction/station.py
@@ -0,0 +1,15 @@
+# Variable Name : "사당", "신도림", "인천공항"
+# Variable Name = Value
+# "사당" -> "신도림" -> "인천공항"
+
+station = "사당행"
+
+print(station + "열차가 들어오고 있습니다.")
+
+station = "신도림행"
+
+print(station + "열차가 들어오고 있습니다.")
+
+station = "인천공항행"
+
+print(station + "열차가 들어오고 있습니다.")
